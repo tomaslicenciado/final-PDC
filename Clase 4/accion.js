5a46554b1af0edb65e15209592ff3f158b32e791
Tomás Ferreyra <tomaslicenciado@gmail.com>
2022-03-31 21:52:51 -0300

    Clase 4 me harté

--- Clase 4/accion.js
+++ Clase 4/accion.js
@@ -1,3 +1,10 @@
-JQuery(document).ready(function(){
-    
+$(document).ready(function(){
+    $("pa1").css("backgroun-color","#0f0");
+    $("pa2").css({color: "#FF0",
+                  backgroundColor: "#000"});
+    $("[name=texto]").append("%");
+    $("[name=parrafos]>p").append("&");
+    var parrafos=$("[id*=pa]").toArray();//busca todos los que tengan "pa" en el id
+    parrafos.forEach(p => {$(p).append("~")});
 })
+

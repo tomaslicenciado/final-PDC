53e049c572e1401f0a963e1c0e0f7ad826e123d3
Tomás Ferreyra <tomaslicenciado@gmail.com>
2022-04-05 09:47:58 -0300

    Clase 5

---
+++ Clase 5/acciones.js
@@ -0,0 +1,20 @@
+/*
+Visto en clases:
+try / catch
+promt
+isNaN para controlar vacío
+alert
+throw new Error ("mensaje de error")
+eval(mensaje)
+with???
+función lambda para recorrer arreglos
+los arreglos pueden definirse sin tamaño e ir agregando elementos
+    var arreglo = new Array(5); Tamaño fijo
+    var arreglo = new Array(); tamaño variable, se pueden agregar todos los elementos que se quiera
+objetos
+*/
+
+
+
+
+//HACER UN RELOJ ANALÓGICO PARA LA PRÓXIMA CLASE!!!!
